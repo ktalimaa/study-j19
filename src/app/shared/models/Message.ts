0ec563e902dc2bcd171c44805587b804600cfd32
ktalimaa <kristeltalimaa@gmail.com>
2023-03-09 21:35:09 +0200

    Snack-bar and other service implementations

---
+++ src/app/shared/models/Message.ts
@@ -0,0 +1,12 @@
+import {MessageType} from "./MessageType";
+
+export class Message {
+  type: MessageType;
+  content: string;
+
+
+  constructor(type: MessageType, content: string) {
+    this.type = type;
+    this.content = content;
+  }
+}
--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SchoolDashboardComponent } from './school-dashboard/school-dashboard.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SchoolDashboardComponent} from './school-dashboard/school-dashboard.component';
 import {MatTableModule} from "@angular/material/table";
-import { CreateSchoolComponent } from './create-school/create-school.component';
-
+import {CreateSchoolComponent} from './create-school/create-school.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     SchoolDashboardComponent,
-    CreateSchoolComponent
+    CreateSchoolComponent,
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
-export class SchoolModule { }
+export class SchoolModule {
+}
