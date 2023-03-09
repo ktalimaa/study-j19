ce91b1da1bda9d01738c09038d7faccc129da8e1
ktalimaa <kristeltalimaa@gmail.com>
2023-03-09 19:46:15 +0200

    Create-school added

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolDashboardComponent } from './school-dashboard/school-dashboard.component';
 import {MatTableModule} from "@angular/material/table";
+import { CreateSchoolComponent } from './create-school/create-school.component';
 
 
 
 @NgModule({
   declarations: [
-    SchoolDashboardComponent
+    SchoolDashboardComponent,
+    CreateSchoolComponent
   ],
   imports: [
     CommonModule,
